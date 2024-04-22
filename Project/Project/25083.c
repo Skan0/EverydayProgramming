f42da02729a99fef34018dc0069063730b145495
Skan0 <sytwj09110@naver.com>
2024-04-22 19:32:10 +0900

    25083

---
+++ Project/Project/25083.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void) {
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+} 
+//         ,r'"7
+//r`-_   ,'  ,/
+// \. ". L_r'
+//   `~\/
+//      |
+//      |
